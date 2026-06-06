050e1eeec2353127a8eb83106ecc07dc9581c471
hnihTyoB <nctmdt@gmail.com>
2026-06-06 23:58:28 +0700

    feat: delete user

--- src/main/java/vn/thinher/quickjob/controller/UserController.java
+++ src/main/java/vn/thinher/quickjob/controller/UserController.java
@@ -1,10 +1,15 @@
 package vn.thinher.quickjob.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.thinher.quickjob.domain.User;
 import vn.thinher.quickjob.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class UserController {
@@ -14,12 +19,14 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public void createNewUser() {
-        User user = new User();
-        user.setName("Thinher");
-        user.setEmail("nctmdt@gmail.com");
-        user.setPassword("123456");
-        userService.handleCreateUser(user);
+    @PostMapping("/users")
+    public String createNewUser(@RequestBody User postManUser) {
+        userService.handleCreateUser(postManUser);
+        return "User created successfully";
+    }
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.handleDeleteUser(id);
+        return "User deleted successfully";
     }
 }
--- src/main/java/vn/thinher/quickjob/service/UserService.java
+++ src/main/java/vn/thinher/quickjob/service/UserService.java
@@ -16,5 +16,7 @@ public class UserService {
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
     }
-
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
 }
