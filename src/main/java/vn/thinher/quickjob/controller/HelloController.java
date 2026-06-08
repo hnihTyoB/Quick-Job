9c6ec68ec68efb72b85ce0525668e2b674710dd6
hnihTyoB <nctmdt@gmail.com>
2026-06-08 22:24:33 +0700

    feat: global exception

--- src/main/java/vn/thinher/quickjob/controller/HelloController.java
+++ src/main/java/vn/thinher/quickjob/controller/HelloController.java
@@ -3,11 +3,16 @@ package vn.thinher.quickjob.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.thinher.quickjob.service.error.IdInvalidException;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String getHelloWorld() {
+    public String getHelloWorld() throws IdInvalidException {
+        if (true) {
+            throw new IdInvalidException("This is an exception");
+        }
         return "Hello World Thinher";
     }
 }
