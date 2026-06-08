b5e4ac2baa8ec7d6718858e4da778054170aa8a5
hnihTyoB <nctmdt@gmail.com>
2026-06-08 17:25:39 +0700

    feat: get & update

--- src/main/java/vn/thinher/quickjob/controller/UserController.java
+++ src/main/java/vn/thinher/quickjob/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.thinher.quickjob.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.thinher.quickjob.domain.User;
 import vn.thinher.quickjob.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -19,10 +22,24 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.handleFetchAllUsers();
+    }
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable("id") long id) {
+        return userService.handleFetchUserById(id);
+    }
+
     @PostMapping("/users")
-    public String createNewUser(@RequestBody User postManUser) {
-        userService.handleCreateUser(postManUser);
+    public String createNewUser(@RequestBody User user) {
+        userService.handleCreateUser(user);
         return "User created successfully";
+        }
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        User existingUser = userService.handleUpdateUser(user);
+        return existingUser;
     }
     @DeleteMapping("/users/{id}")
     public String deleteUser(@PathVariable("id") long id) {
--- src/main/java/vn/thinher/quickjob/service/UserService.java
+++ src/main/java/vn/thinher/quickjob/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.thinher.quickjob.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.thinher.quickjob.domain.User;
@@ -12,9 +15,30 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    public List<User> handleFetchAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleFetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
 
     public User handleCreateUser(User user) {
         return this.userRepository.save(user);
+    }
+    public User handleUpdateUser(User user) {
+        User existingUser = this.handleFetchUserById(user.getId());
+        if (existingUser != null) {
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
+            existingUser = this.userRepository.save(existingUser);
+        }
+        return existingUser;
     }
     public void handleDeleteUser(long id) {
         this.userRepository.deleteById(id);
