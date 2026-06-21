58d3169b5f739d96fd58bce43377e639e3192f24
hnihTyoB <nctmdt@gmail.com>
2026-06-21 22:13:40 +0700

    feat: RequestMapping control version

--- src/main/java/vn/thinher/quickjob/controller/CompanyController.java
+++ src/main/java/vn/thinher/quickjob/controller/CompanyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ import vn.thinher.quickjob.service.CompanyService;
 import vn.thinher.quickjob.util.annotation.ApiMessage;
 
 @RestController
+@RequestMapping("/api/v1")
 public class CompanyController {
 
     private final CompanyService companyService;
